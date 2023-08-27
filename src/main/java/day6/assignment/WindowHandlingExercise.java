ea198408ee574af845f5a70636fb1a9e6ed6e3ac
RajalakshmiGR <rajalakshmisdet@gmail.com>
2023-08-27 20:36:30 +0530

    Aug25 HWdone

--- src/main/java/day6/assignment/WindowHandlingExercise.java
+++ src/main/java/day6/assignment/WindowHandlingExercise.java
@@ -1,6 +1,9 @@
 package day6.assignment;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -22,11 +25,29 @@ public class WindowHandlingExercise {
 
 // Launch URL  //input[@id=""]   //input[@id="password"] //input[@id="Login"]
 		driver.get("https://login.salesforce.com");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.findElement(By.id("username")).sendKeys("hari.radhakrishnan@qeagle.com");
 		driver.findElement(By.id("password")).sendKeys("Leaf$1234");
 		driver.findElement(By.id("Login")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'Learn More')]")).click();
+		
+		Set<String> windowHandles = driver.getWindowHandles();
+		List<String> windows= new ArrayList<String>(windowHandles);
+		
+		System.out.println(windows.get(0));
+		System.out.println(windows.get(1));
+		
+		driver.switchTo().window(windows.get(1));
+		driver.findElement(By.xpath("//button[contains(text(),'Confirm')]")).click();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		String Child_Window_title = driver.switchTo().window(windows.get(1)).getTitle();
+		System.out.println("Child_Window_title is - "+Child_Window_title);
+		driver.close();
+		
+		String Parent_Window_title = driver.switchTo().window(windows.get(0)).getTitle();
+		System.out.println("Parent_Window_title is - "+Parent_Window_title);
+		driver.close();
+			
 
 	}
 
