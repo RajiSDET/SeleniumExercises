53bc88d352cb9dd4a93858282c188e7563338108
RajalakshmiGR <rajalakshmisdet@gmail.com>
2023-08-30 06:59:00 +0530

    Aug28 HWdone

---
+++ src/main/java/day8/assignment/WaitExercise.java
@@ -0,0 +1,24 @@
+package day8.assignment;
+
+public class WaitExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/*
+ 
+Day_9:
+
+Step:1  Launch the browser and Load url
+https://www.leafground.com/window.xhtml
+
+Step:2 Click on the button Open with delay
+      apply explicitWait->numberofWindowsTobe
+Step:3 Get the title of second window
+
+Step:4 Quit browser
+ */
